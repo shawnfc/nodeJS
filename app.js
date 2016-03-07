aff6b87b684f2bbdb8b8e373af4d51407570b7a8
Shawn Conrad <shawn_conrad@homedepot.com>
2016-03-07 08:39:04 -0500

    Lecture 17: First-class Functions and Function Expressions

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-var a = 1;
-var b = 2;
-var c = a + b;
+//function statement
+function greet () {
+    console.log('Hi!');
+}
+greet();
 
-console.log(c);
+//functions are first-class
+function logGreeting (fn) {
+    fn()
+}
+logGreeting(greet);
+
+//function expression
+var greetMe = function () {
+    console.log('Yo, Shawn!');
+}
+greetMe();
+
+// it's first-class
+logGreeting(greetMe);
+
+// use a function expression on the fly
+logGreeting(function () {
+    console.log("What up, Freshco?");
+});
