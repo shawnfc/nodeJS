40b7f1403535bdcbec47c7ccb86874462c307088
Shawn Conrad <shawn_conrad@homedepot.com>
2016-03-07 08:50:41 -0500

    Lecture 18: Build a Module

--- app.js
+++ app.js
@@ -1,25 +1,2 @@
-//function statement
-function greet () {
-    console.log('Hi!');
-}
+var greet = require('./greet');
 greet();
-
-//functions are first-class
-function logGreeting (fn) {
-    fn()
-}
-logGreeting(greet);
-
-//function expression
-var greetMe = function () {
-    console.log('Yo, Shawn!');
-}
-greetMe();
-
-// it's first-class
-logGreeting(greetMe);
-
-// use a function expression on the fly
-logGreeting(function () {
-    console.log("What up, Freshco?");
-});
